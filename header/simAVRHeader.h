132a02d17ca6faaf20a9dd8f50c2b3ba165205cc
Smit Modi <smodi005@ucr.edu>
2020-01-12 11:25:32 -0800

    added mmcu struct

--- header/simAVRHeader.h
+++ header/simAVRHeader.h
@@ -12,7 +12,8 @@ AVR_MCU_VCD_FILE("build/results/lab2_intoToAVR_trace.vcd",1000);
 const struct avr_mmcu_vcd_trace_t _mytrace[] _MMCU_ = {
     { AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0,.what = (void*)&PINA, } , // Example individual pin
     { AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, } , // Example full port
-};
+    { AVR_MCU_VCD_SYMBOL("PINA"), .what = (void*)&PINA, } , // Line added here
+;
 
 /* Function to output through UART */
 static int uart_putchar(char c, FILE *stream) {
